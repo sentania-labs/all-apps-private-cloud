57a77bb75f5a8e6e605003762b57738ea790574a
Scott R Bowe <scottb@sentania.net>
2025-12-17 10:22:07 -0600

    fmt

--- projects.tf
+++ projects.tf
@@ -1,6 +1,7 @@
 module "projects" {
-  source   = "sentania-labs/projects/vcfa"
+  source  = "sentania-labs/projects/vcfa"
   version = "0.1.0"
+
   for_each = var.projects
 
   project_name = each.value.name
